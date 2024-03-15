93599a204b5842c1b571231e4b21d26d81203360
feindreh <119032218+feindreh@users.noreply.github.com>
2024-03-15 02:48:54 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
-
-
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  
+  const [hello,setHello] = useState("Bye")
+  
+  const handler = (str) => {
+    setHello(str)
+  }
+
   return (
     <main>
-      <button>
+      <div>{hello}</div>
+      <button onClick = {()=>{handler("Bye")}}>
         Leiter Anschauen
       </button>
-      <button>
+      <button onClick = {()=>{handler("Hello")}}>
         Spiel Eintragen
       </button>
     </main>
