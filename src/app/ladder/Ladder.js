36097c48dbedc0f58fd0232229761b2168868d0a
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-16 22:21:45 +0200

    add Ladder

---
+++ src/app/ladder/Ladder.js
@@ -0,0 +1,29 @@
+
+
+const SingleEntry = (props) => {
+
+    const elo = props.player.elo
+    const name = props.player.name
+    const place = props.place
+    
+    return(
+        <div style={{display:"flex",gap:"10px"}}>
+            <div>{place}.</div>
+            <div>{name}</div>
+            <div>{elo}</div>
+        </div>
+        )
+
+}
+
+
+export default function Ladder(props){
+    return (
+        <>
+            <div>Ich bin Die Ladder</div>
+            <div>
+                {props.players.map((player,i) => <SingleEntry key={player.name} player = {player} place = {i+1}/>)}
+            </div>
+        </>
+    )
+}
