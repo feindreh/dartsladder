4f062fcb06e00cf7c501374069c25c6a14250c34
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-16 22:05:33 +0200

    add  load data on mount

--- src/app/ladder/page.js
+++ src/app/ladder/page.js
@@ -1,6 +1,25 @@
+"use client"
+
 import HomeButton from "../elements/home"
+import { getLadder } from "../firebase/addData"
+
+import { useEffect,useState } from "react"
 
 export default function Hello(){
+
+    const [players,SetPlayers] = useState([])
+
+    const loadData = async() => {
+        const data = await getLadder()
+        data.sort((a,b) => b.elo - a.elo)
+        SetPlayers(data)
+    }
+
+    useEffect(()=>{
+        loadData()
+    },[])
+
+
     return (
         <div>
             <HomeButton/>
