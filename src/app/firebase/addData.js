09771942c8c77adcee254ddaf63b2150437377b0
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-18 00:26:27 +0200

    add firebase functions

---
+++ src/app/firebase/addData.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+import { getFirestore } from "firebase/firestore";
+import {collection, query, getDocs, doc, setDoc ,getDoc} from "firebase/firestore"; 
+
+import app from "./hmm";
+
+const db = getFirestore(app);
+
+const addName = async (name) => {
+    // Add a new document in collection "cities"
+    await setDoc(doc(db, "users", name), {
+        "name": name,
+        "elo": 1200
+    });
+}
+
+export const hasName = async (name) => {
+    const docRef = doc(db, "users", name);
+    const docSnap = await getDoc(docRef);
+    if(docSnap.data() === undefined){return false}
+    return true
+}
+
+export const getLadder = async () => {
+    
+    const q = query(collection(db, "users"));
+    const querySnapshot = await getDocs(q);
+    const arr = []
+    querySnapshot.forEach((doc) => (arr.push(doc.data())))
+    return arr
+}
+
+export default addName
