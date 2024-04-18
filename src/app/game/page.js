6d88a4248beb6922db4449f7beea7221556e09db
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-18 16:25:09 +0200

    add layout for game page

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,10 +1,27 @@
+"use client"
+
 import HomeButton from "../elements/home"
 
 export default function Hello(){
+
+    const handleClick = () => {
+        console.log("HI")
+    }
+
+
     return (
         <div>
             <HomeButton/>
             <div>game</div>
+            <div style={{display:"flex",flexDirection:"column",gap:"10px",alignItems:"center"}}>
+                <div>Player1</div>
+                <input type="text" />
+                <div>Player2</div>
+                <input type="text" />
+                <div>Result</div>
+                <input type="text" />
+                <button type="button" onClick = {()=>{handleClick()}}>Submit</button>
+            </div>
         </div>  
     )
 }
