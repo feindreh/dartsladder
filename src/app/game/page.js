56e85d53987dd252931e831a6b3e7acfec45ef95
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-24 16:31:15 +0200

    delete game layout

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,32 +1,18 @@
 "use client"
 
-import { useState } from "react"
 
 import HomeButton from "../elements/home"
-import Player from "./player"
 
 export default function Hello(){
 
 
-    const [players,setPlayers] = useState([])
-
-    const newPlayer = () => {
-        setPlayers([...players,""])
-    }
-
-    const deletePlayer = (index) => {
-        setPlayers(players.filter((a,i) => i !== index))
-    }
+    
 
 
     return (
         <div>
             <HomeButton/>
             <div>game</div>
-            <div>
-                {players.map((a,i) => <  Player key = {i} delete = {deletePlayer} index = {i}/>)}
-                <button type="button" onClick = {()=>newPlayer()}>New Player</button>
-            </div>
             
             
         </div>  
