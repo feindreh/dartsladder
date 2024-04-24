cd06054587d4effe32e7571ebc5458a47d9a7f54
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-24 16:35:35 +0200

    add game layout

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -7,14 +7,29 @@ export default function Hello(){
 
 
     
-
+    const handleButton = () => {
+        console.log("hi")
+    }
 
     return (
         <div>
             <HomeButton/>
             <div>game</div>
-            
-            
+            <div>
+                <div>Player 1</div>
+                <input type="text"></input>
+            </div>
+            <div>
+                <div>Player 2</div>
+                <input type="text"></input>
+            </div>
+            <div>
+                <div>Winner {"(check = Player 1 else Player 2)"}</div>
+                <input type="checkbox"></input>
+            </div>
+            <div>
+                <button type="button" onClick = {()=>{handleButton()}}>Sumbit</button>
+            </div>
         </div>  
     )
 }
