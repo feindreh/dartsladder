cad86042749728ebab0dcc22caf8e5a30e6018c7
feindreh <119032218+feindreh@users.noreply.github.com>
2024-04-24 17:01:17 +0200

    add form to input 2 player + result

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -2,34 +2,46 @@
 
 
 import HomeButton from "../elements/home"
+import { useState } from "react";
+
 
 export default function Hello(){
 
-
+    const [player1,setPlayer1] = useState("Flx")
+    const [player2,setPlayer2] = useState("Cll")
+    const [win,setWin] = useState()
     
-    const handleButton = () => {
-        console.log("hi")
+    const handleButton = (e) => {
+        e.preventDefault();
+        console.log(player1,player2,win)
     }
 
     return (
         <div>
             <HomeButton/>
             <div>game</div>
-            <div>
-                <div>Player 1</div>
-                <input type="text"></input>
-            </div>
-            <div>
-                <div>Player 2</div>
-                <input type="text"></input>
-            </div>
-            <div>
-                <div>Winner {"(check = Player 1 else Player 2)"}</div>
-                <input type="checkbox"></input>
-            </div>
-            <div>
-                <button type="button" onClick = {()=>{handleButton()}}>Sumbit</button>
-            </div>
+            <form onSubmit = {handleButton}>
+
+                <div>
+                    <label>Player 1</label>
+                    <input type="text" value={player1} onChange = {(e)=>{setPlayer1(e.target.value)}}></input>
+                </div>
+                
+                <div>
+                    <label>Player 2</label>
+                    <input type="text" value= {player2} onChange = {(e)=>{setPlayer2(e.target.value)}}></input>
+                </div>
+
+                <div>
+                    <label>Winner</label>
+                    <input type="radio" name="group1" value="player1" onChange = {(e)=>{setWin(e.target.value)}}></input>
+                    <input type="radio" name="group1" value="player2" onChange = {(e)=>{setWin(e.target.value)}}></input>
+                </div>
+                
+                <button type="submit">Sumbit</button>
+             
+            </form>
+            
         </div>  
     )
 }
