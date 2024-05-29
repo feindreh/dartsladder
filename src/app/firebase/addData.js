93db0e1f585a12c1c1aa33652e0e1f57a72d3ecd
feindreh <119032218+feindreh@users.noreply.github.com>
2024-05-29 04:49:29 +0200

    add test data

--- src/app/firebase/addData.js
+++ src/app/firebase/addData.js
@@ -10,30 +10,35 @@ import app from "./hmm";
 
 const db = getFirestore(app);
 
+const main = "users"
+const second = "dusers"
+
+const inUse = second
+
 const addName = async (name) => {
     // Add a new document in collection "cities"
-    await setDoc(doc(db, "users", name), {
+    await setDoc(doc(db, inUse, name), {
         "name": name,
         "elo": 1200
     });
 }
 
 export const updateElo = async (name,elo) => {
-    await setDoc(doc(db,"users",name),{
+    await setDoc(doc(db,inUse,name),{
         "name":name,
         "elo":elo
     })
 }
 
 export const hasName = async (name) => {
-    const docRef = doc(db, "users", name);
+    const docRef = doc(db, inUse, name);
     const docSnap = await getDoc(docRef);
     if(docSnap.data() === undefined){return false}
     return true
 }
 
 export const getPlayer = async (name) => {
-    const docRef = doc(db, "users", name);
+    const docRef = doc(db, inUse, name);
     const docSnap = await getDoc(docRef);
     if(docSnap.data() === undefined){return false}
     return docSnap.data()
@@ -41,7 +46,7 @@ export const getPlayer = async (name) => {
 
 export const getLadder = async () => {
     
-    const q = query(collection(db, "users"));
+    const q = query(collection(db, inUse));
     const querySnapshot = await getDocs(q);
     const arr = []
     querySnapshot.forEach((doc) => (arr.push(doc.data())))
