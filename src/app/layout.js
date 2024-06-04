3884f7bfea4859e7f7de217ca9bd6938df8e554a
feindreh <119032218+feindreh@users.noreply.github.com>
2024-06-04 22:45:48 +0200

    move container div into layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body >
+        <main className="container">
+          {children}
+       </main>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,29 +4,23 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <main className="container">
-      <div className ="container2">
-
-        <Link href ="/ladder">
-          <button className="MainButton">
-            Leiter anschauen
-          </button>
-        </Link>
-
-        <Link  href="/game">
-          <button className="MainButton">
-            Spiel eintragen
-          </button>
-        </Link>
-
-        <Link  href="/player">
-          <button className="MainButton">
-            Neuer Spieler
-          </button >
-        </Link>
-
-      </div>
-    </main>
+        <div className = "container2">
+          <Link href ="/ladder">
+            <button className="MainButton">
+              Leiter anschauen
+            </button>
+          </Link>
+          <Link  href="/game">
+            <button className="MainButton">
+              Spiel eintragen
+            </button>
+          </Link>
+          <Link  href="/player">
+            <button className="MainButton">
+              Neuer Spieler
+            </button >
+          </Link>
+        </div>
   );
 }
 
