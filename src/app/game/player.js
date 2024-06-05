4b7c4a480dcd7dca249cacb90b6de99ca196e028
feindreh <119032218+feindreh@users.noreply.github.com>
2024-06-05 15:49:08 +0200

    add css to player

--- src/app/game/player.js
+++ src/app/game/player.js
@@ -1,8 +1,18 @@
 export default function Player (props) {
 
-                return (<div>
-                    <label>Player {props.index + 1 }</label>
-                    <input type="text" value={props.name} onChange = {(e)=>{props.callback(e.target.value,props.index)}}></input>
-                    <button type="button" onClick = { () => {props.delete(props.index)}}>Delete</button>
-                </div>)
+    return (
+    <div className = {"player"}>
+
+        <label>
+            Spieler {props.index + 1 }
+        </label>
+
+        <input type="text" value={props.name} onChange = {(e)=>{props.callback(e.target.value,props.index)}}>
+        </input>
+
+        <button className = {"box"} type="button" onClick = { () => {props.delete(props.index)}}>
+            Löschen
+        </button>
+
+    </div>)
 }
